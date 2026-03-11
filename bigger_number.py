f5ba5947f936139bc79da4c213a226aacfd87c6f
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-11 17:26:10 +0800

    final_code

--- bigger_number.py
+++ bigger_number.py
@@ -1,2 +1,9 @@
 num1 = int(input("Enter first_number: "))
 num2 = int(input("Enter second_number: "))
+
+if num1 > num2:
+    print("The bigger number is", num1)
+elif num2 > num1:
+    print("The bigger number is", num2)
+else:
+    print("Both number are equal.")
