36ad3f6c0e08445abceb28aaf343dc5bc93eddca
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 01:33:43 +0800

    Added a custom function to replicate Python’s title() behavior without using the built-in method.

---
+++ functionality_of_title.py
@@ -0,0 +1,24 @@
+def custom_title(text):
+    words = text.split(" ")
+    result_words = []
+
+    for word in words:
+        if word:  
+            first_char = word[0]
+            rest = word[1:]
+
+            if 'a' <= first_char <= 'z':
+                first_char = chr(ord(first_char) - 32)
+
+            new_rest = ""
+            for char in rest:
+                if 'A' <= char <= 'Z':
+                    new_rest += chr(ord(char) + 32)
+                else:
+                    new_rest += char
+
+            result_words.append(first_char + new_rest)
+        else:
+            result_words.append("")
+
+    return " ".join(result_words)
