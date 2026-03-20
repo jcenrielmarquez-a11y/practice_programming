d5cb16bf7969eb8e50ba4e7b93684d6ba08a789d
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 01:00:49 +0800

    Added character-checking logic to ensure all alphabetic characters are uppercase.

--- functionality_of_isupper.py
+++ functionality_of_isupper.py
@@ -7,3 +7,7 @@ def custom_isupper(text):
             return False
     return True
 
+sample1 = "HELLO WORLD"
+sample2 = "Hello World"
+print("Sample1:", repr(sample1), "->", custom_isupper(sample1))
+print("Sample2:", repr(sample2), "->", custom_isupper(sample2))
