5be3ff1581cb988d8532cefae23babe2d330663d
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 01:19:34 +0800

    Added balanced space-padding logic to align strings centrally within a specified width.

--- functionality_of_center.py
+++ functionality_of_center.py
@@ -5,3 +5,7 @@ def custom_center(text, width):
     left_spaces = total_spaces // 2
     right_spaces = total_spaces - left_spaces
     return " " * left_spaces + text + " " * right_spaces
+
+sample = "Hello"
+print("Original:", repr(sample))
+print("After custom_center:", repr(custom_center(sample, 11)))
