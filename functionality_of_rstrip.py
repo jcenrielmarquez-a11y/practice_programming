632136c613744c15ec9762ab94dc74b8e0bbe99b
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 09:54:42 +0800

    Added backward traversal logic to strip trailing spaces while preserving leading ones.

--- functionality_of_rstrip.py
+++ functionality_of_rstrip.py
@@ -5,3 +5,9 @@ def custom_rstrip(text):
         index -= 1
     return text[:index + 1]
 
+sample1 = "Hello World    "
+sample2 = "NoTrailingSpace"
+print("Original:", repr(sample1))
+print("After custom_rstrip:", repr(custom_rstrip(sample1)))
+print("Original:", repr(sample2))
+print("After custom_rstrip:", repr(custom_rstrip(sample2)))
