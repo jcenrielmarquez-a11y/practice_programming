01354427ca20fa13c4488cb1c76b4237b8ab660b
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 10:07:12 +0800

    Added ASCII-based logic to convert lowercase letters into uppercase equivalents.

--- functionality_of_upper.py
+++ functionality_of_upper.py
@@ -6,3 +6,10 @@ def custom_upper(text):
         else:
             result += char
     return result
+
+sample1 = "Hello World!"
+sample2 = "python123"
+print("Original:", repr(sample1))
+print("After custom_upper:", repr(custom_upper(sample1)))
+print("Original:", repr(sample2))
+print("After custom_upper:", repr(custom_upper(sample2)))
