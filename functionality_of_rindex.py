f105a69a1bb80ed2a52b3880798e432caa31e48c
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 11:19:52 +0800

    Added a custom function to replicate Python’s rindex() behavior without using the built-in method.

---
+++ functionality_of_rindex.py
@@ -0,0 +1,9 @@
+def custom_rindex(text, sub):
+    sub_len = len(sub)
+
+    for i in range(len(text) - sub_len, -1, -1):
+
+        if text[i:i + sub_len] == sub:
+            return i
+
+    raise ValueError(f"Substring '{sub}' not found in '{text}'")
