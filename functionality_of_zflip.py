8959c1e64f230b940fa8690450bd5e8c1db77053
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 10:48:43 +0800

    Added zero-padding logic to extend strings to the specified width.

--- functionality_of_zflip.py
+++ functionality_of_zflip.py
@@ -3,3 +3,10 @@ def custom_zfill(text, width):
         return text
     zeros_to_add = width - len(text)
     return "0" * zeros_to_add + text
+
+sample1 = "123"
+sample2 = "Hello"
+print("Original:", repr(sample1))
+print("After custom_zfill:", repr(custom_zfill(sample1, 6)))
+print("Original:", repr(sample2))
+print("After custom_zfill:", repr(custom_zfill(sample2, 8))) 
