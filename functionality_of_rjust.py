cab17fb344763bcd890703ee1e65285c37844e2d
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-21 10:35:06 +0800

    Added space-padding logic to align strings to the right within a specified width.

--- functionality_of_rjust.py
+++ functionality_of_rjust.py
@@ -3,3 +3,10 @@ def custom_rjust(text, width):
         return text
     spaces_to_add = width - len(text)
     return " " * spaces_to_add + text
+
+sample1 = "Hello"
+sample2 = "World"
+print("Original:", repr(sample1))
+print("After custom_rjust:", repr(custom_rjust(sample1, 10)))
+print("Original:", repr(sample2))
+print("After custom_rjust:", repr(custom_rjust(sample2, 8)))
